2e40d82cfb083f17b9679e51200e2e92b58ac702
CDTiLab <CDTiLab@UBSCOF202-02>
2025-05-12 07:55:54 -0500

    subida del proyecto back al git

---
+++ proyectoFinalC3/src/main/java/co/edu/unbosque/proyectoFinalC3/ProyectoFinalC3Application.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.proyectoFinalC3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalC3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalC3Application.class, args);
+	}
+
+}
